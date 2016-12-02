f9e0528eabefe8263c70be3018c25cceaaa78153
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-02 17:08:15 -0200

    Starting the project struct, it's only a hello word.

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from os import listdir, getcwd, path
+from importlib import import_module
+from re import sub
+from flask import Flask
+from flask.ext.runner import Manager
+from inflection import singularize
+
+flask = Flask(__name__, template_folder=path.join(getcwd(), __name__, 'views'))
+
+manager = Manager(flask)
+
+
+def register_blueprints(flask, package):
+    package_dir = path.join(getcwd(), __name__, package)
+	module_suffix = '_' + singularize(package) + '.py'
+
+	module_names = [sub('\.py', '', c)
+					for c in listdir(package_dir) if c.endswith(module_suffix)]
+
+	for module_name in module_names:
+		module = import_module(__name__ + '.%s.%s' % (package, module_name))
+		flask.register_blueprint(module.blueprint)
+
+register_blueprints(flask, 'controllers')
+register_blueprints(flask, 'apis')
