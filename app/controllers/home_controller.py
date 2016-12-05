10ee7f1c8327b4f57f6a291a9dd16e112f066661
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-05 19:06:13 -0200

    Correcting the routes in home

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -2,12 +2,12 @@ from flask import Blueprint, render_template
 
 blueprint = Blueprint('home_controller', __name__, url_prefix='/')
 
-@blueprint.route("/")
+@blueprint.route('')
 def index():
 	return render_template('home/index.html')
 
 
-@blueprint.route("about")
+@blueprint.route('about')
 def about():
 	return render_template('home/about.html')
 
