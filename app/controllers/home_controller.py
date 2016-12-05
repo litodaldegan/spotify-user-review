2ca02d399c8956a0d7f97ea2be3aa259bcb9c159
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-05 15:41:49 -0200

    Adding about page

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -5,3 +5,9 @@ blueprint = Blueprint('home_controller', __name__, url_prefix='/')
 @blueprint.route("/")
 def index():
 	return render_template('home/index.html')
+
+
+@blueprint.route("about")
+def about():
+	return render_template('home/about.html')
+
