ea736a1c58cef84875d3c13a2ef1c5ff703f74ad
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-12 18:41:27 -0200

    Add the environment to test

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,  Integer, String
+from sqlalchemy import Column,  Integer, String, Boolean,ForeignKey
 from app import db
 
 
@@ -23,3 +23,15 @@ class User(db.Model):
 		yield 'followers', self.followers
 		yield 'product', self.product
 
+
+class PlayList(db.Model):
+	__tablename__ = 'playlist'
+
+	id = Column(Integer, primary_key=True)
+	user = Column('user_id', Integer, ForeignKey("user.id"), nullable=False)
+	playlist_id = Column(String)
+	owner = Column(String)
+	name = Column(String)
+	url = Column(String)
+	image = Column(String)
+	public = Column(Boolean)
