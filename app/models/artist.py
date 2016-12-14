8bb36805c39c1e9d69c8c38a4775fea59cd8a2d0
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-14 17:38:26 -0200

    Add models to artist.

--- app/models/artist.py
+++ app/models/artist.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column,  Integer, String, Boolean,ForeignKey
+from sqlalchemy import Column, Integer,ForeignKey, String
 from app import db
 
 class Artist(db.Model):
-	__tablename__ = 'playlist'
+	__tablename__ = 'artist'
 
 	id = Column(Integer, primary_key=True)
 	user = Column('user_id', Integer, ForeignKey("user.id"), nullable=False)
-
 	name = Column(String)
 	spotify_id = Column(String)
-	img = Column(String)
+	image = Column(String)
 	url = Column(String)
+	popularity = Column(Integer)
+	genres = Column(String)
+
