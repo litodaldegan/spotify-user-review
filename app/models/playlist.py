5425733eeb063057108a3e2d68a568513114a490
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2016-12-15 11:48:39 -0200

    put a serializer in playlist models.

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from app import db
+
 
 class PlayList(db.Model):
 	__tablename__ = 'playlist'
@@ -12,3 +13,11 @@ class PlayList(db.Model):
 	url = Column(String)
 	image = Column(String)
 	public = Column(Boolean)
+
+	def __iter__(self):
+		yield 'playlist_id', self.playlist_id
+		yield 'owner', self.owner
+		yield 'name', self.name
+		yield 'url', self.url
+		yield 'image', self.image
+		yield 'public', self.public
