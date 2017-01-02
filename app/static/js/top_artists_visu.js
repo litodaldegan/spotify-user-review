ebd067c0c5c3b9effd690b3c0a250c14c6bf186b
Italo Daldegan de Oliveira <italodaldegan@hotmail.com>
2017-01-02 18:47:07 -0200

    Add artist photo to tooltip.

--- app/static/js/top_artists_visu.js
+++ app/static/js/top_artists_visu.js
@@ -1,5 +1,7 @@
 var mount_top_artists_graph = function (user_id) {
 
+	var htmlButton = "<a id='google' href='http://www.google.com' target='_blank'>Click here to go to Google</a>"
+
 	$.ajax({
 	        dataType: 'json',
 	        type: 'GET',
@@ -20,6 +22,23 @@ var mount_top_artists_graph = function (user_id) {
 	                    .y({"value": "popularity", "label": "Popularity", "range": [0, 100], "grid": {"color": "#333"}})
 	                    .background("#232323")
 	                    .axes({"background": {"color": "#232323"}})
+	                    .tooltip({
+	                    	"stacked": true,
+	                    	"large": 250,
+	                    	"html": function(d){
+
+	                    	for (i = 0; i < response.json_data.length; i++){
+	                    		if (response.json_data[i].name == d){
+	                    			artist = response.json_data[i];
+			                    	var show_info = '<div><a href=' + artist.url +
+			                    		' target="_blank"><img style="width: 250px;height:250px;float:left;display:block" src=' +
+			                    		artist.image + ' alt="See on spotify"></a></div>';
+	                    		}
+	                    	}
+	                    	
+	                    	return show_info;
+	                    	}
+	                    })
 	                    .draw()
 	        }
 	})
